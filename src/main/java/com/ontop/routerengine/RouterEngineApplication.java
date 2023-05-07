8b6a14dec28e4390b56d5536e9ae0d726bb89aa1
kanyatta98 <patrickkimani273@gmail.com>
2023-05-07 07:36:09 +0300

    feat: initial commit

---
+++ src/main/java/com/ontop/routerengine/RouterEngineApplication.java
@@ -0,0 +1,13 @@
+package com.ontop.routerengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ontop/routerengine/RouterEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontop.routerengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
